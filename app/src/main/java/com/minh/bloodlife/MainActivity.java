09d64df6bd7a5e5ec2b455b8ad8f6f319b5ab7f5
MinT <tnm1042002@gmail.com>
2024-12-14 22:28:33 +0700

    delete Database Helper

--- app/src/main/java/com/minh/bloodlife/MainActivity.java
+++ app/src/main/java/com/minh/bloodlife/MainActivity.java
@@ -2,13 +2,39 @@ package com.minh.bloodlife;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.minh.bloodlife.ui.login.LoginActivity;
+import com.minh.bloodlife.ui.registration.RegistrationActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser == null){
+            // No user is signed in, redirect to LoginActivity
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish(); // Finish MainActivity so the user can't come back to it by pressing the back button
+        } else {
+            // User is signed in, stay on MainActivity
+            // You might want to load user data here or perform other actions
+        }
     }
 }
